e95751a7c792a92f906b63292facd19fc9bb0936
Berend Gábor <berendg@inf.u-szeged.hu>
2018-03-02 08:25:10 +0100

    checks for the query word to be in the freq list prior including it

--- run_experiments.sh
+++ run_experiments.sh
@@ -8,7 +8,7 @@ do
     do
       for m in sparse-feats not-sparse-feats;
       do
-         python evaluate_dag.py --subtask $1 --dense_archit $a --sparse_dimensions $d --sparse_density 0.${s} --not-filter-candidates --not-save-gpickle >> ${1}_full${2}.log 2>&1
+         nice -n6 python evaluate_dag.py --subtask $1 --dense_archit $a --sparse_dim $d --sparse_density 0.${s} --filter-candidates --not-save-gpickle >> ${1}_${a}_${s}_${d}_${m}.log 2>&1 &
       done
     done
   done
