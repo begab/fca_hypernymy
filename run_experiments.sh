a6de07c104744a2876fb81b9db770c0dcb000823
Berend Gábor <berendg@inf.u-szeged.hu>
2018-03-04 09:47:55 +0100

    makes subtask parametrizable for the ablational experiments

--- run_experiments.sh
+++ run_experiments.sh
@@ -8,7 +8,7 @@ do
     do
       for m in sparse-feats not-sparse-feats;
       do
-         nice -n6 python evaluate_dag.py --subtask $1 --dense_archit $a --sparse_dim $d --sparse_density 0.${s} --filter-candidates --not-save-gpickle ${m} >> ${1}_${a}_${s}_${d}_${m}.log 2>&1 &
+         nice -n6 python evaluate_dag.py --subtask $1 --dense_archit $a --sparse_dim $d --sparse_density 0.${s} --negative_samples 50 --not-filter-candidates --not-save-gpickle --${m} >> ${1}_FULL_${a}_${s}_${d}_${m}_50.log 2>&1 &
       done
     done
   done
--- table1experiments.sh
+++ table1experiments.sh
@@ -1,6 +1,13 @@
 #/!bin/bash
 
-for t in 1A 1B 1C 2A 2B;
+if [[ $# -lt 1 ]];
+then
+    SUBTASK="1A 1B 1C 2A 2B"
+else
+    SUBTASK=$1
+fi
+
+for st in $SUBTASK;
 do
     for sd in 200 300;
     do
@@ -10,7 +17,7 @@ do
             do
                 for d in not-dag-feats dag-feats;
                 do
-                    python evaluate_dag.py --subtask $t --dense_archit $v --sparse_dim $sd --negative_samples 50 --sparse_density 0.3 --${s} --filter-candidates --${d} --not-save-gpickle >> table1experiments.log 2>&1;
+                    python evaluate_dag.py --subtask $st --dense_archit $v --sparse_dim $sd --negative_samples 50 --sparse_density 0.3 --${s} --filter-candidates --${d} --not-save-gpickle >> table1experiments_${st}.log 2>&1;
                 done
             done
         done
